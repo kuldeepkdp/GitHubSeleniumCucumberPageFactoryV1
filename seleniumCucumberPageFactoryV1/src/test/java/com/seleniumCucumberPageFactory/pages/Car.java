21d0a8e7dc74334924ff91fbf3d9395bd47993b0
kuldeep kumar <kuldeep.kdp@gmail.com>
2018-09-06 16:18:40 +0530

    added designcar method

--- seleniumCucumberPageFactoryV1/src/test/java/com/seleniumCucumberPageFactory/pages/Car.java
+++ seleniumCucumberPageFactoryV1/src/test/java/com/seleniumCucumberPageFactory/pages/Car.java
@@ -3,9 +3,14 @@ package com.seleniumCucumberPageFactory.pages;
 public class Car {
 	
 	String colour="red";
+	String model="abc";
 	
 	public void startCar() {
 		System.out.println("Car is started");
+	}
+	
+	public void designCar() {
+		System.out.println("Car is designed");
 	}
 
 }
